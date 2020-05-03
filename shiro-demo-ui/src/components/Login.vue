7f103f826fcac27ed0e16522c1f3358d9a2e2464
wuhepeng <wuhepeng@changingedu.com>
2020-05-03 23:53:03 +0800

    ui prepared, for login & index page, use element UI

---
+++ shiro-demo-ui/src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="background-color: thistle">
+    <el-input v-model="username" placeholder="用户名" size="medium" class="my_input"></el-input>
+    <br>
+    <el-input v-model="password" placeholder="密码" size="medium" class="my_input"></el-input>
+    <br>
+    <br>
+    <el-checkbox style="color: black;margin-right: 20px">记住我</el-checkbox>
+    <el-button type="primary" @click="doLogin"> Log In </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'Login',
+  data () {
+    return {
+      'username': '',
+      'password': ''
+    }
+  },
+  methods: {
+    doLogin () {
+      this.$router.push('/index')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .my_input {
+    width: 200px;
+  }
+</style>
--- shiro-demo-ui/src/main.js
+++ shiro-demo-ui/src/main.js
@@ -4,12 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入elementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
